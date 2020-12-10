971df8afacd93d5cb375edd0d3285e7382d73a10
BhuvanSetty <72972241+bsetty013@users.noreply.github.com>
2020-12-10 16:35:06 +0000

    Delete Text To Speech Learning.py

--- Continuos Assessment 2/Initial Files/Text To Speech Learning.py
+++
@@ -1,9 +0,0 @@
-import win32com.client as wincl
-speak = wincl.Dispatch("SAPI.SpVoice")
-speak.Speak("Hello World")
-
-
-import pyttsx3
-engine = pyttsx3.init()
-engine.say("I will speak this text")
-engine.runAndWait()
